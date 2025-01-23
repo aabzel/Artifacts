d49cd04b13c02ce72af43b0a498c444265e355e9
aabdev <aabzel@yandex.ru>
2025-01-23 23:56:23 +0300

    + Flash Jlink scripts

---
+++ olimex_h407_bootloader_gcc_m/build_1/flash_bootloader_by_J_link.bat
@@ -0,0 +1,16 @@
+cls
+::echo off
+
+set flash_tool="C:\Program Files (x86)\SEGGER\JLink\JFlash.exe"
+set mcu_config=STM32F407ZG_JTAG.jflash
+set firmware=olimex_h407_bootloader_gcc_m.hex
+
+set option=
+set option=%option% -openprj%mcu_config% 
+set option=%option% -open%firmware% 
+set option=%option% -programverify 
+set option=%option% -startapp  
+set option=%option% -exit
+
+%flash_tool% %option% 
+
